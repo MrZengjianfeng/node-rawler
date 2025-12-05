c2de6d8a442a62f04b7627c580b9f79a16e9af56
beiqi <2313482869@qq.com>
2025-12-05 17:57:48 +0800

    feat: 新加淘宝cookies生成器

--- src/crawlers/taobao.js
+++ src/crawlers/taobao.js
@@ -1,4 +1,5 @@
 import { limitedFetchPost } from "../utils/requestUtil.js";
+import { generateTaobaoCookies } from "../utils/cookieUtil.js";
 
 /**
  * 发送淘宝注册短信验证码请求
@@ -50,8 +51,7 @@ async function sendTaobaoSms(mobile) {
     };
 
     // 添加Cookie信息
-    const cookies =
-      "XSRF-TOKEN=d41f7d04-c649-4469-986d-09f270b4935b; cookie2=1df01dd33692badbafea23cbc347b713; t=5cfc7aafda3b9fafcd26a854ce8dd9f8; _tb_token_=f07383d98b78b; thw=cn; 3PcFlag=1764559855772; wk_cookie2=15aed05bc2b31885b32d9c6ad52956d0; wk_unb=UUjTSzAVnZKbvw%3D%3D; lid=%E9%A3%9E%E9%9B%AA%E7%89%87%E7%89%87%E5%87%9D%E7%91%9E; wk_cookie2=15aed05bc2b31885b32d9c6ad52956d0; wk_unb=UUjTSzAVnZKbvw%3D%3D; aui=2021950341; mtop_partitioned_detect=1; _m_h5_tk=0e908033bc5c753bb21da5e8dcc480cf_1764935772559; _m_h5_tk_enc=673b30385172880fdbfd5b6db1b21f7d; _samesite_flag_=true; sdkSilent=1764954133810; havana_sdkSilent=1764954133810; xlly_s=1; fastSlient=1764925336667; sgcookie=E100mtbsp8iSR%2FKsBR61t1QB7vLc4E8UFgJl16xAxqSs4%2F7%2Frg4raksKq1XIKT7d23%2Bgowv81WbyBy4q66y45xfzBwPNf7zuV26cBzHIOmh%2B6qlPHS2ZyuSDqzeW1mwhSKZq; x5sec=7b2274223a313736343932353531312c22733b32223a2231396632306662343733633133343036222c22617365727665723b33223a22307c434d504179736b47454e4c56772f5146476777794d4449784f5455774d7a51784f7a4577753632516f2f2f2f2f2f2f2f41513d3d227d; sca=cc7e6401; mt=ci=0_0; tracknick=; cna=7fmzIWZ5EV4CAXARVHca2F7T; regsid=bcc7f5a4fee0aeddd390dd14e83fa7ca2ce5da5a; regsc=3d827e7481cd052718dd788a1a7e5a1015fd415e0b1dcbdb0604362515117236; isg=BObmSD8ss0OODmf4CxGqI3ZeN1xoxyqBBTA2YdCPUInkU4dtOFfakdkgr09ffSKZ; _bl_uid=L1mO5i0Csjsn5LejFv05b7m1nwL8; arms_uid=fe2b0866-00db-441f-abfb-4308832b3fa2; tfstk=gmMj5EM3SZbbUIhLGR-PO0RH7M2_h3JUXGZtxcBVX-eY6OgLzO-03C2s5zgguSyxQR1t74-0uc-mP53E-mW47nVtPcgIumz47ONTPqBagAiGClntSn4wmQommR298ek6Lmm0zIvvUx1Yegnub1E9aSCfCzay8eJeLII8CETE05LWg8EgyoETXRK7woqLB1FtWzF8vk7YBR39VzE4vOUOBtpRwk48WRetW0K7bzEYBR3t23ZiB0T7qIaxcH45-6WW2UiaPOBtFuLgrmTfhkkYcSaKDYXOBhqbGynYPEOcrYF-j5MGjONZDX0gvqQv5PDS2xhKJU54kXZsYfgpbsqz1x27WxphAzHjfYNmaZCYA-ab9xFpCHiL6bM73xLGt0o7kWwraQY4_-gjtyVvZU0sVruth7QXzPlE4xFSJUWSS5Gt38HvypsyZ9zIPZf1VWX_Vy-WV1fG5JhB701Gy-FYqukeV3_fsSEuVy-WV1fgMuqza3t5l1f.";
+    const cookies = generateTaobaoCookies();
     headers["cookie"] = cookies;
 
     // 使用带超时的Promise确保不会无限期阻塞
---
+++ src/test/cookieTest.js
@@ -0,0 +1,4 @@
+import { generateTaobaoCookies } from "../utils/cookieUtil.js";
+
+console.log("Generated Taobao Cookies:");
+console.log(generateTaobaoCookies());
